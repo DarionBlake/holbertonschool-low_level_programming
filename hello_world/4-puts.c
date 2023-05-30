776cf380ee9624089d89360abea9a078b44ca8ab
DarionBlake <thasupabooking@gmail.com>
2023-05-30 09:03:34 -0700

    L

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry Point
  *
- * return: always 0 (success)
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+return (0);
 }
