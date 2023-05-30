ae0f46a2438d30d68230bc05656c9dcaac4aabb9
DarionBlake <thasupabooking@gmail.com>
2023-05-30 09:27:13 -0700

    words in

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	printf("Size of char: %zu\n", sizeof(char));
 	printf("Size of short int: %zu\n", sizeof(short int));
-	printf("size pf int: %zu\n", sizeof(int));
+	printf("size of int: %zu\n", sizeof(int));
 	printf("Size of long int: %zu\n", sizeof(long int));
 	printf("Size of float: %zu\n", sizeof(float));
 	printf("Size of double: %zu\n", sizeof(double));
