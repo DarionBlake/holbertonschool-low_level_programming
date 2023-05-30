eb94e8244918122dc9713d8ec04a685425d06a1c
DarionBlake <thasupabooking@gmail.com>
2023-05-30 09:24:02 -0700

    L

---
+++ hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: always i0 (success)
+ */
+
+int main(void)
+{
+	printf("Size of char: %zu\n", sizeof(char));
+	printf("Size of short int: %zu\n", sizeof(short int));
+	printf("size pf int: %zu\n", sizeof(int));
+	printf("Size of long int: %zu\n", sizeof(long int));
+	printf("Size of float: %zu\n", sizeof(float));
+	printf("Size of double: %zu\n", sizeof(double));
+	printf("Size of long double: %zu\n", sizeof(long double));
+return (0);
+}
