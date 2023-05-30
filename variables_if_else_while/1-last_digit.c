153b567eae9bf8003007d056f8af7b9a52db6e54
DarionBlake <thasupabooking@gmail.com>
2023-05-30 13:16:57 -0700

    L

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * 
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	return (0);
+}
