e0196ec47c5212eedadee05ff5357b1d251bc697
DarionBlake <thasupabooking@gmail.com>
2023-06-01 12:49:05 -0700

    L

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+	int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		  printf("%d\n", i);
+	}
+	return (0);
+	}
