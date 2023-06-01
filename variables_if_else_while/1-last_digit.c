23ceabcf45d8beac59f738ca7802168117211369
DarionBlake <thasupabooking@gmail.com>
2023-06-01 12:00:57 -0700

    L

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -8,9 +8,27 @@
  */
 int main(void)
 {
-	int n = 0;
+	int n;
+	int lastdigit;
 
 	srand(time(0));
-	n = rand(0) - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
+	lastdigit = n % 10;
+	printf("Last digit of ");
+	printf("%i", n);
+	printf(" is %i ", lastdigit);
+	if (lastdigit > 5)
+	{
+		printf("and is greater than 5");
+	}
+	else if (lastdigit == 0)
+	{
+		printf("and is 0");
+	}
+	else
+	{
+		printf("and is less than 6 and not 0");
+	}
+	printf("\n");
 	return (0);
 }
