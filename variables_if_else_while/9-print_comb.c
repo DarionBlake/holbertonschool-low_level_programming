770edce302e0cd0afece8952054e9a69e177cf4e
DarionBlake <thasupabooking@gmail.com>
2023-06-02 09:55:27 -0700

    L

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,18 +6,19 @@
  */
 
 int main(void)
-
 {
 	int l;
 
 	for (l = 48; l <= 57; l++)
-
-	putchar (l);
-if (l <= 57)
 	{
-				putchar(',');
-				putchar(' ');
-				putchar('\n');
-			}
+		putchar (l);
+		if (l <= 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	
 	return (0);
 }
