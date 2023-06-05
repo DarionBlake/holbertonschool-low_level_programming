f3bdc80db96664844b6ea51b6dcfdfa3055cde45
DarionBlake <thasupabooking@gmail.com>
2023-06-05 10:29:55 -0700

    L

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code.
+ * @c: interger value
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int product = a * b;
+	return product;
+}
