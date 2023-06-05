a84ddc327a010e170d011a0e70396dfcb73a8606
DarionBlake <thasupabooking@gmail.com>
2023-06-05 13:03:00 -0700

    L

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints 0 to 9
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+	{
+	for (i = 0; 1 <= 9; i++)
+		if (i == 2 || i == 4)
+			return (0);
+	}
+	{
+		_putchar('0' + i)
+	}
+	_putchar('\n');
+}
