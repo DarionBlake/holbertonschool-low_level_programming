24d60c49ea0818b8ef586c1bda484b5a3162250f
DarionBlake <thasupabooking@gmail.com>
2023-06-06 09:13:04 -0700

    L

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,14 +9,14 @@
  */
 void print_most_numbers(void)
 {
-	int k;
+	int k = 0;
 	{
-		for (k = 0; 1 <= 9; k++)
-
+		if (k < 10)
 		{
 			if ((k != 2) && (k != 4))
 				_putchar('0' + k);
 		}
-	_putchar('\n');
+		k += 1;
 	}
+	_putchar('\n');
 }
