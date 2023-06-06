ddb6d186d7593c4da5ed9768d4cbc879ad773a0d
DarionBlake <thasupabooking@gmail.com>
2023-06-06 11:25:28 -0700

    L

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -5,11 +5,11 @@ void print_line(int n)
 {
 	if (n <= 0)
 	{
-		_putchar('\n');
+		putchar('\n');
 	}
 	for (int i = 0; i < n; i++)
 	{
-		_putchar('_');
+		putchar('_');
 	}
-	_putchar('\n');
+	putchar('\n');
 }
