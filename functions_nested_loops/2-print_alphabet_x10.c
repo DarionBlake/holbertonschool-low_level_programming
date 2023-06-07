f71b626b1785c24d8ee9763c6efe770ecaf3da23
DarionBlake <thasupabooking@gmail.com>
2023-06-07 10:11:07 -0700

    L

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - check the code.
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
-}
-	int = i;
+	int i, c;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (char c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
