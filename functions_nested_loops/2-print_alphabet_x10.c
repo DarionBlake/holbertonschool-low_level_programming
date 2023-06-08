f90a974dc5994707dfad2c01cf1055aeb3e8d108
DarionBlake <thasupabooking@gmail.com>
2023-06-08 08:29:42 -0700

    L

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void print_alphabet_x10(void)
+{
+	int i = 10;
+
+	for (int i = 0; i < 9; i++)
+	{
+		char k = 'a';
+
+		while ( r <= 'z')
+		{
+			if (k >= 'a' && i <= 'j')
+			{
+                _putchar(0);
+            }
+
+            i++;
+        }
+    }
+
+    _putchar('\n');
+}
