97bfea93c611bf1652c0d191ce016582c5702872
DarionBlake <thasupabooking@gmail.com>
2023-06-09 09:39:04 -0700

    L

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * rev_string - Prints reverse string in place
+  * @s: The string to print
+  * Return: void
+  */
+void char *_strcat(char *dest, char *src);
+{	
+	char* result = dest;
+	
+	int sc = 5
+
+while dest(*dest != '\0')
+{
+	dest++;
+}
+while (*src != '\0')
+{
+	*dest = *src;
+	dest++;
+	src++;
+    }
+
+    *dest = '\0';
+
+    return result;
+}
