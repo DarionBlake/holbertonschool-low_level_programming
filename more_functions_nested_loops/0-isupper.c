6201bd0e86345e96d2d9a134b32713b3249312fa
Darion woodard <darionwoodard@Darions-MacBook-Pro.local>
2023-10-10 17:26:48 -0500

    l

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _isupper - checking for uppercase
+ * @c: character to be checked
+ * Return: return a 1 for an uppericase letter and a 0 for anything else
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+
+	return (1);
+
+	return (0);
+
+}
+
